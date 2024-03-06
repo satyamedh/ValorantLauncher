421ae5aca4ac041b946a5aa23d321fb6f3f5bb04
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2024-03-06 16:34:47 +0530

    Initial commit

---
+++ VALORANT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+string command = "C:\\Riot Games\\Riot Client\\RiotClientServices.exe";
+string arguements = "--launch-product=valorant --launch-patchline=live";
+
+Process process = new Process();
+
+process.StartInfo.FileName = command;
+process.StartInfo.Arguments = arguements;
+process.StartInfo.UseShellExecute = false;
+
+Console.WriteLine("Launching Valorant");
+
+process.Start();
+
+Console.WriteLine("Executed launch command");
+
+process.WaitForExit();
+
